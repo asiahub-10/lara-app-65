205d3229796b8fb93e68312cf1c13783632ecfac
asiahub-10 <asiarahman104@gmail.com>
2025-10-26 13:06:58 +0600

    database: migration, factory, seeder

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    protected $table = 'ecom_roles';
+    use HasFactory;
+    protected $table = 'roles';
     
 }
